34dfb01df9a7e90dba39425a53754b477ee3d3d8
iherrera <ivan@39inc.com>
2017-09-05 12:04:31 -0300

    Purchase Detail

--- ios11/ClientList/ClientList/Attachment.cs
+++ ios11/ClientList/ClientList/Attachment.cs
@@ -3,14 +3,21 @@ using UIKit;
 using Foundation;
 namespace ClientList
 {
-    public class Attachment
+    public class Attachment: NSObject
     {
+
+        public static string ApplicationGroup = "group.com.example.xamarin-samplecode.ClientList";
+
+        public static string AttachmentName = "Attachment.jpg";
+
+        public static string PurposeIdentifier = "com.example.xamarin-samplecode.ClientList";
+
         public Attachment()
         {
         }
 
         public static NSUrl Url(string name){
-            return NSUrl.FromString("http://www.microsoft.com");
+            return NSUrl.FromString("http://www.xamarin.com");
         }
     }
 }
--- ios11/ClientList/ClientList/PurchaseDetailViewController.cs
+++ ios11/ClientList/ClientList/PurchaseDetailViewController.cs
@@ -1,11 +1,14 @@
 ﻿using System;
-
+using Foundation;
 using UIKit;
 
 namespace ClientList
 {
     public partial class PurchaseDetailViewController : UIViewController
     {
+
+        public NSUrl Url;
+
         public PurchaseDetailViewController() : base("PurchaseDetailViewController", null)
         {
         }
@@ -14,6 +17,29 @@ namespace ClientList
         {
             base.ViewDidLoad();
             // Perform any additional setup after loading the view, typically from a nib.
+            View.BackgroundColor = UIColor.White;
+            var imageview = new UIImageView();
+            imageview.ContentMode = UIViewContentMode.ScaleAspectFit;
+            View.AddSubview(imageview);
+            imageview.TranslatesAutoresizingMaskIntoConstraints = false;
+            imageview.LeadingAnchor.ConstraintEqualTo(View.LeadingAnchor, 20).Active = true;
+            imageview.TrailingAnchor.ConstraintEqualTo(View.TrailingAnchor, -20).Active = true;
+            imageview.TopAnchor.ConstraintEqualTo(View.TopAnchor, 100).Active = true;
+            imageview.BottomAnchor.ConstraintEqualTo(View.BottomAnchor, -10).Active = true;
+
+            var fileCoordinator = new NSFileCoordinator();
+            NSError error;
+            fileCoordinator.PurposeIdentifier = Attachment.PurposeIdentifier;
+            fileCoordinator.CoordinateRead(Url, NSFileCoordinatorReadingOptions.WithoutChanges, out error, (NSUrl url) => {
+                NSError err;
+                var imageData = NSData.FromUrl(url, NSDataReadingOptions.Uncached, out err);
+                if (err == null){
+                    imageview.Image = new UIImage(imageData);
+                }else{
+                    imageview.Image = null;
+                }
+            });
+
         }
 
         public override void DidReceiveMemoryWarning()
