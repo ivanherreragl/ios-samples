66545e20c1ed347da089b0a5fdd4eac1028f38b6
iherrera <ivan@39inc.com>
2017-09-07 11:13:33 -0300

    Attachment functions

--- ios11/ClientList/ClientList/Attachment.cs
+++ ios11/ClientList/ClientList/Attachment.cs
@@ -12,12 +12,44 @@ namespace ClientList
 
         public static string PurposeIdentifier = "com.example.xamarin-samplecode.ClientList";
 
+        public static NSUrl DirectoryURL(){
+            return NSFileManager.DefaultManager.GetContainerUrl(ApplicationGroup).Append("File Provider Storage", true);
+        }
+
         public Attachment()
         {
         }
 
         public static NSUrl Url(string name){
-            return NSUrl.FromString("http://www.xamarin.com");
+            return DirectoryURL().Append(name, true).Append(AttachmentName, true);
         }
+
+        public static void Load(NSUrl url){
+            var name = url.RemoveLastPathComponent().LastPathComponent;
+            var resourceName = name + " Data";
+            var bundleURL = NSBundle.MainBundle.GetUrlForResource(resourceName, "jpg");
+
+
+
+            NSError err;
+            NSFileAttributes fileAttributes = new NSFileAttributes();
+            fileAttributes.PosixPermissions = Convert.ToInt16("755", 8);
+            NSFileManager.DefaultManager.CreateDirectory(url.RemoveLastPathComponent().Path, true, fileAttributes, out err );
+
+            NSError err2;
+            NSFileManager.DefaultManager.Copy(bundleURL, url, out err2);
+
+			NSError err3;
+			NSFileAttributes fileAttributes2 = new NSFileAttributes();
+			fileAttributes2.PosixPermissions = Convert.ToInt16("644", 8);
+            NSFileManager.DefaultManager.SetAttributes(fileAttributes, url.Path, out err3);
+
+        }
+
+        public static string Identifier(NSUrl url){
+            return url.RemoveLastPathComponent().LastPathComponent;
+        }
+
+
     }
 }
