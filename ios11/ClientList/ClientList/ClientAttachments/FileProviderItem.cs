fc3bf5fd4b9c455df5e028d81eb445710c5c60b2
iherrera <ivan@39inc.com>
2017-09-07 11:43:13 -0300

    File Provider Item

--- ios11/ClientList/ClientList/ClientAttachments/FileProviderItem.cs
+++ ios11/ClientList/ClientList/ClientAttachments/FileProviderItem.cs
@@ -1,10 +1,72 @@
 ﻿using System;
-namespace ClientList.ClientAttachments
+using FileProvider;
+using UIKit;
+using Foundation;
+using MobileCoreServices;
+
+namespace ClientAttachments
 {
-    public class FileProviderItem
+    public class FileProviderItem: NSObject, INSFileProviderItem
     {
+        
+
+		private string _identifier = string.Empty;
+
+        public NSString Identifier
+		{
+			get
+			{
+                return (NSString)_identifier;
+			}
+			set
+			{
+				_identifier = value;
+			}
+		}
+
+
+		public string TypeIdentifier
+		{
+			get
+			{
+				return (string)UTType.JPEG;
+			}
+
+		}
+
+        public NSFileProviderItemIdentifier ParentItemIdentifier(){
+            return NSFileProviderItemIdentifier.RootContainer;
+        }
+
+        public NSString ParentIdentifier
+		{
+			get
+			{
+                return (NSString)ParentItemIdentifier().ToString();
+			}
+
+		}
+
+        public string Filename
+		{
+			get
+			{
+                return _identifier + " Attachment.jpg";
+			}
+
+		}
+
+
+        public NSFileProviderItemCapabilities Capabilities = NSFileProviderItemCapabilities.All;
+
+
         public FileProviderItem()
         {
         }
+
+		public FileProviderItem(string name)
+		{
+            _identifier = name;
+		}
     }
 }
