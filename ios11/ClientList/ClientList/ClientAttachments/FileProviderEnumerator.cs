50de593701b89f657162ffd39449a77cb46db5b0
iherrera <ivan@39inc.com>
2017-09-08 09:19:45 -0300

    Rest of code

--- ios11/ClientList/ClientList/ClientAttachments/FileProviderEnumerator.cs
+++ ios11/ClientList/ClientList/ClientAttachments/FileProviderEnumerator.cs
@@ -6,8 +6,32 @@ namespace ClientList
 {
     public class FileProviderEnumerator: NSObject, INSFileProviderEnumerator
     {
+
+        public NSFileProviderItemIdentifier EnumeratedItemIdentifier;
+
         public FileProviderEnumerator()
         {
         }
+
+		public FileProviderEnumerator(NSFileProviderItemIdentifier enumeratedItemIdentifier)
+		{
+            EnumeratedItemIdentifier = enumeratedItemIdentifier;
+
+		}
+
+        public void Invalidate(){
+            
+        }
+
+        public void EnumerateItems(INSFileProviderEnumerationObserver observer, NSData page){
+            //if (page == )
+        }
+
+        public void EnumerateChanges(INSFileProviderEnumerationObserver observer, NSData anchor){
+
+            observer.FinishEnumerating(anchor);
+            
+        }
+
     }
 }
--- ios11/ClientList/ClientList/ClientAttachments/FileProviderExtension.cs
+++ ios11/ClientList/ClientList/ClientAttachments/FileProviderExtension.cs
@@ -1,10 +1,57 @@
 ﻿using System;
-namespace ClientList.ClientAttachments
+using FileProvider;
+using Foundation;
+using UIKit;
+
+namespace ClientList
 {
-    public class FileProviderExtension
+    public class FileProviderExtension: NSFileProviderExtension
     {
+
+        NSFileManager FileManager = new NSFileManager();
+
         public FileProviderExtension()
         {
         }
+
+        public INSFileProviderItem Item(NSFileProviderItemIdentifier identifier){
+            return new FileProviderItem(identifier.ToString());
+        }
+
+        public NSUrl UrlForItem(NSFileProviderItemIdentifier identifier){
+            return Attachment.Url(identifier.ToString());
+        }
+
+        public NSString PersistentIdentifierForItem(NSUrl url){
+            return (NSString)Attachment.Identifier(url);
+
+        }
+
+        public void TrashItem(NSString itemIdentifier, Action<INSFileProviderItem, NSError> completionHandler){
+            NSError err;
+            NSFileManager.DefaultManager.Remove(Attachment.Url(itemIdentifier).RemoveLastPathComponent(), out err);
+            if (err == null){
+                completionHandler(null, null);
+            }else{
+                completionHandler(null, err);
+            }
+        }
+
+        public void DeleteItem(NSString itemIdentifier, Action<NSError> completionHandler){
+            completionHandler(null);
+        }
+
+        public INSFileProviderEnumerator Enumerator (NSString containerItemIdentifier){
+            INSFileProviderEnumerator maybeEnumerator = null;
+            if (containerItemIdentifier == NSFileProviderItemIdentifier.RootContainer.ToString()){
+                return new FileProviderEnumerator(NSFileProviderItemIdentifier.RootContainer);
+            }else if (containerItemIdentifier == NSFileProviderItemIdentifier.WorkingSetContainer.ToString()){
+                return new FileProviderEnumerator(NSFileProviderItemIdentifier.WorkingSetContainer);
+            } //Need .allDirectories as well
+
+            return maybeEnumerator;
+				
+        }
+
     }
 }
